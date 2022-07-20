cd5aaf6372064b6112692fa27a3be3c8b98e949d
akhilhb <109649690+akhilhb@users.noreply.github.com>
2022-07-20 07:43:18 +0100

    Add files via upload

--- Azurevm.tf
+++ Azurevm.tf
@@ -1,17 +1,27 @@
-  resource "azurerm_VM" "some_erver" {
-    administrator_login          = "MY_login"
-    administrator_login_password = "MY_password"
-    location                     ="Central_india"
-    name                         = "Windows_server_2016"
-    resource_group_name          = "MY_resource"
-  
+resource "azurerm_resource_group" "example" {
+  name     = "tfex-recovery_vault"
+  location = "West US"
+}
+
+resource "azurerm_recovery_services_vault" "example" {
+  name                = "tfex-recovery-vault"
+  location            = "${azurerm_resource_group.example.location}"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  sku                 = "Standard"
+}
+
+resource "azurerm_recovery_services_protection_policy_vm" "test" {
+  name                = "tfex-recovery-vault-policy"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  recovery_vault_name = "${azurerm_recovery_services_vault.example.name}"
+
+  timezone = "UTC"
 
   backup {
     frequency = "Daily"
-    time      = "04:00"
+    time      = "23:00"
   }
 
   retention_daily {
-    count = 30
+    count = 10
   }
-}
